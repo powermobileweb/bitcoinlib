e7bb04c4017c4954374a6618607fd8b388445788
PowerMobile Team <info@powermobile.org>
2018-08-23 21:52:53 +0200

    Release 1.9.0

---
+++ src/BitcoinLib/RPC/Specifications/RpcMethods.cs
@@ -0,0 +1,120 @@
+﻿// Copyright (c) 2014 - 2016 George Kimionis
+// See the accompanying file LICENSE for the Software License Aggrement
+
+namespace BitcoinLib.RPC.Specifications
+{
+    //  Note: Do not alter the capitalization of the enum members as they are being cast as-is to the RPC server
+    public enum RpcMethods
+    {
+        //== Blockchain ==
+        getbestblockhash,
+        getblock,
+        getblockchaininfo,
+        getblockcount,
+        getblockhash,
+        getblockheader,
+        getchaintips,
+        getdifficulty,
+        getmempoolinfo,
+        getrawmempool,
+        gettxout,
+        gettxoutproof,
+        gettxoutsetinfo,
+        verifychain,
+        verifytxoutproof,
+
+        //== Control ==
+        getinfo,
+        help,
+        stop,
+
+        //== Generating ==
+        generate,
+        getgenerate,
+        setgenerate,
+
+        //== Mining ==
+        getblocktemplate,
+        getmininginfo,
+        getnetworkhashps,
+        prioritisetransaction,
+        submitblock,
+
+        //== Network ==
+        addnode,
+        clearbanned,
+        disconnectnode,
+        getaddednodeinfo,
+        getconnectioncount,
+        getnettotals,
+        getnetworkinfo,
+        getpeerinfo,
+        listbanned,
+        ping,
+        setban,
+
+        //== Rawtransactions ==
+        createrawtransaction,
+        decoderawtransaction,
+        decodescript,
+        fundrawtransaction,
+        getrawtransaction,
+        sendrawtransaction,
+        signrawtransaction,
+        sighashtype,
+
+        //== Util ==
+        createmultisig,
+        estimatefee,
+        estimatepriority,
+        estimatesmartfee,
+        estimatesmartpriority,
+        validateaddress,
+        mirroraddress,
+        verifymessage,
+
+        //== Wallet ==
+        abandontransaction,
+        addmultisigaddress,
+        addwitnessaddress,
+        backupwallet,
+        dumpprivkey,
+        dumpwallet,
+        getaccount,
+        getaccountaddress,
+        getaddressesbyaccount,
+        getbalance,
+        getnewaddress,
+        getrawchangeaddress,
+        getreceivedbyaccount,
+        getreceivedbyaddress,
+        gettransaction,
+        getunconfirmedbalance,
+        getwalletinfo,
+        importaddress,
+        importprivkey,
+        importpubkey,
+        importwallet,
+        keypoolrefill,
+        listaccounts,
+        listaddressgroupings,
+        listlockunspent,
+        listreceivedbyaccount,
+        listreceivedbyaddress,
+        listsinceblock,
+        listtransactions,
+        listmirrtransactions,
+        listunspent,
+        lockunspent,
+        move,
+        sendfrom,
+        sendmany,
+        sendtoaddress,
+        setaccount,
+        settxfee,
+        signmessage,
+        walletlock,
+        walletpassphrase,
+        walletpassphrasechange
+    }
+}
---
+++ src/BitcoinLib/Services/Coins/Bitcoin/BitcoinService.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) 2014 - 2016 George Kimionis
+// See the accompanying file LICENSE for the Software License Aggrement
+
+using BitcoinLib.CoinParameters.Bitcoin;
+
+namespace BitcoinLib.Services.Coins.Bitcoin
+{
+    public class BitcoinService : CoinService, IBitcoinService
+    {
+        public BitcoinService(bool useTestnet = false) : base(useTestnet)
+        {
+        }
+
+        public BitcoinService(string daemonUrl, string rpcUsername, string rpcPassword, string walletPassword)
+            : base(daemonUrl, rpcUsername, rpcPassword, walletPassword)
+        {
+        }
+
+        public BitcoinService(string daemonUrl, string rpcUsername, string rpcPassword, string walletPassword, short rpcRequestTimeoutInSeconds)
+            : base(daemonUrl, rpcUsername, rpcPassword, walletPassword, rpcRequestTimeoutInSeconds)
+        {
+        }
+
+        public BitcoinConstants.Constants Constants => BitcoinConstants.Constants.Instance;
+    }
+}
